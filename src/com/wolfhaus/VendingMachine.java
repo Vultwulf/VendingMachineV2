2769ec9341980449a106cc3f2b091b70a016b26c
swolfenb <swolfenbarger@bodhiserver.info>
2018-06-22 14:49:27 -0400

    Initial commit

---
+++ src/com/wolfhaus/VendingMachine.java
@@ -0,0 +1,22 @@
+package com.wolfhaus;
+
+/**
+ * The VendingMachine program implements an application that
+ * sastifies the requirements of the kata-vending-machine
+ * project.
+ *
+ * https://github.com/PillarTechnology/kata-vending-machine
+ *
+ * @author  Scott Wolfenbarger
+ * @version 1.0
+ * @since   2018-06-22
+ */
+public class VendingMachine {
+
+    /**
+     * Constructor for VendingMachine
+     */
+    public VendingMachine(){
+
+    }
+}
---
+++ test/com/wolfhaus/VendingMachineTest.java
@@ -0,0 +1,15 @@
+package com.wolfhaus;
+
+/**
+ * The VendingMachineTest implements all test-cases for use
+ * of Test Driven Development for the VendingMachine program.
+ *
+ * https://github.com/PillarTechnology/kata-vending-machine
+ *
+ * @author  Scott Wolfenbarger
+ * @version 1.0
+ * @since   2018-06-22
+ */
+public class VendingMachineTest {
+
+}
